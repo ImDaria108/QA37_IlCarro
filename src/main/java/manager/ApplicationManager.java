d220d448b0f3d3ae93f13eea857ad9864fa612af
ImDaria108 <daria.immerman@gmail.com>
2023-04-15 20:29:38 +0300

    Logger

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,12 +3,15 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 
 public class ApplicationManager {
 
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
     WebDriver wd;
     HelperUser helperUser;
     HelperCar helperCar;
@@ -18,9 +21,11 @@ public class ApplicationManager {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         wd = new ChromeDriver(options);
+        logger.info("All tests run in Chrome brauser");
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wd.navigate().to("https://ilcarro.web.app/");
+        logger.info("The link --->" + wd.getCurrentUrl());
 
         helperUser =new HelperUser(wd);
         helperCar = new HelperCar(wd);
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -12,50 +12,61 @@ public class LoginTests extends TestBase{
     public void preCondition(){
         if(app.getHelperUser().isLogged()){
             app.getHelperUser().logout();
+            logger.info("Before method finish logout");
         }
     }
 
     @Test
     public void loginSuccess(){
+        logger.info("Start test with name 'loginSuccess'");
+        logger.info("Test data: ---> email: 'dasha@gmail.com' & password 'Ashtanga8!'");
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("dasha@gmail.com","Ashtanga8!");
         app.getHelperUser().submit();
 
         Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+        logger.info("Assert check is Information window 'Logged in success' present");
 
     }
 
-
     @Test
     public void loginSuccessModel(){
+        logger.info("Test data: ---> email: 'dasha@gmail.com' & password 'Ashtanga8!'");
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("dasha@gmail.com","Ashtanga8!");
         app.getHelperUser().submit();
 
         Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+        logger.info("Assert check is Information window 'Logged in success' present");
 
     }
     @Test
     public void loginWrongEmail(){
+        logger.info("Test data: ---> email: '...' & password 'Ashtanga8'");
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("...","Ashtanga8!");
         app.getHelperUser().submit();
         Assert.assertEquals(app.getHelperUser().getErrorText(),"It'snot look like email");
         Assert.assertTrue(app.getHelperUser().isYallaButtonNotActive());
+        logger.info("Assert check is alert present with error text  'It'snot look like email'");
     }
     @Test
     public void loginWrongPassword(){
+        logger.info("Test data: ---> email: 'dasha@gmail.com' & password 'Ashtanga8'");
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("dasha@gmail.com","Ashtanga8");
         app.getHelperUser().submit();
         Assert.assertEquals(app.getHelperUser().getMessage(),"\"Login or Password incorrect\"");
+        logger.info("Assert check is alert present with error text 'Login or Password incorrect'");
     }
     @Test
     public void loginUnregistered(){
+        logger.info("Test data: ---> email: 'cat@mail.com' & password 'Mouse666@'");
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("cat@mail.com","Mouse666@");
         app.getHelperUser().submit();
         Assert.assertEquals(app.getHelperUser().getMessage(),"\"Login or Password incorrect\"");
+        logger.info("Assert check is alert present with error text  'Login or Password incorrect'");
     }
     @AfterMethod
     public void postCondition(){
